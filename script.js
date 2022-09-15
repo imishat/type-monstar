ad2fc30623fea53c16c940a6c70fc7870407f170
MacBook <macbook@MacBooks-MacBook-Air.local>
2022-09-15 19:57:06 +0600

    solved timetaken

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ const gameOver = () => {
     // so total time taken is current time - start time
     const finishTime = new Date().getTime();
     const timeTaken = (finishTime - startTime) / 1000;
+    const timeTakeInte = parseInt(timeTaken)
 
     // show result modal
     resultModal.innerHTML = "";
@@ -82,7 +83,7 @@ const gameOver = () => {
     // show result
     resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTakeInte}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
